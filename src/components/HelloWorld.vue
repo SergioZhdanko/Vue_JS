2e6cb3b4aca4d3e130a489369fcc48e829580849
serjio <baghi@yandex.ru>
2021-05-13 17:49:18 +0400

    key display & checkbox added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,27 @@
     <input v-model.number="operand2" />
     = {{ result }}
     <div>
-      <button @click="calcualte('+')">+</button>
-      <button @click="calcualte('-')">-</button>
-      <button @click="calcualte('/')">/</button>
-      <button @click="calcualte('*')">*</button>
-      <button @click="calcualte('%')">%</button>
-      <button @click="calcualte('^')">^</button>
+      <button v-for="op in operations" :key="op" @click="calcualte(op)">
+        {{ op }}
+      </button>
+    </div>
+    <div class="error" v-if="error">
+      {{ error }}
+    </div>
+    <input type="checkbox" id="checkbox" v-model="isDisplay" />
+    <label for="checkbox"></label>Отобразить экранную клавиатуру привязка нажиме
+    <!-- привязка нажимаемых кнопок к полям ввода не позволяет вводить цифры в несколько разрядов, цифры вводятся как символы, стирание крайней цифры не работает-->
+    <div class="key-display" v-show="isDisplay">
+      <button v-for="digit in digits" :key="digit" @click="operand1 = digit">
+        {{ digit }}
+      </button>
+      <!-- выбор операндов пока не работает -->
+      <div class="selectionOperand">
+        <input type="radio" id="one" value="Первый операнд" v-model="picked" />
+        <label for="one">Первый операнд</label>
+        <input type="radio" id="two" value="Второй операнд" v-model="picked" />
+        <label for="two">Второй операнд</label>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +35,10 @@ export default {
     operand1: 0,
     operand2: 0,
     result: 0,
+    operations: ["+", "-", "/", "*", "%", "^"],
+    error: "",
+    digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<-"],
+    isDisplay: false,
   }),
   props: {
     msg: String,
@@ -27,26 +46,20 @@ export default {
 
   methods: {
     calcualte(op) {
-      switch (op) {
-        case "+":
-          this.result = this.operand1 + this.operand2;
-          break;
-        case "-":
-          this.result = this.operand1 - this.operand2;
-          break;
-        case "/":
-          this.result = this.operand1 / this.operand2;
-          break;
-        case "*":
-          this.result = this.operand1 * this.operand2;
-          break;
-        case "%":
-          this.result = this.operand1 % this.operand2;
-          break;
-        case "^":
-          this.result = Math.pow(this.operand1, this.operand2);
-          break;
+      const { operand1, operand2 } = this;
+      if (op === "/" && operand2 === 0) {
+        this.error = "Division by zero!";
+        return;
       }
+      const calcOperation = {
+        "+": () => operand1 + operand2,
+        "-": () => operand1 - operand2,
+        "/": () => operand1 / operand2,
+        "*": () => operand1 * operand2,
+        "%": () => operand1 % operand2,
+        "^": () => Math.pow(operand1, operand2),
+      };
+      this.result = calcOperation[op]();
     },
   },
 };
@@ -67,5 +80,10 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.error {
+  padding: 20px;
+  border: 1px solid red;
 }
 </style>
