0a29659b09a96e99da32f2ccd9e123f30f763fda
serjio <baghi@yandex.ru>
2021-05-15 13:40:46 +0400

    digits of number input,last digit erasing, selection of input fields have been added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,23 +7,36 @@
       <button v-for="op in operations" :key="op" @click="calcualte(op)">
         {{ op }}
       </button>
+      <button @click="(operand1 = 0), (operand2 = 0)">Clear</button><br />
     </div>
     <div class="error" v-if="error">
       {{ error }}
     </div>
     <input type="checkbox" id="checkbox" v-model="isDisplay" />
-    <label for="checkbox"></label>Отобразить экранную клавиатуру привязка нажиме
-    <!-- привязка нажимаемых кнопок к полям ввода не позволяет вводить цифры в несколько разрядов, цифры вводятся как символы, стирание крайней цифры не работает-->
+    <label for="checkbox"></label>Отобразить экранную клавиатуру
     <div class="key-display" v-show="isDisplay">
-      <button v-for="digit in digits" :key="digit" @click="operand1 = digit">
+      <button v-for="digit in digits" :key="digit" @click="addNumber(digit)">
         {{ digit }}
       </button>
-      <!-- выбор операндов пока не работает -->
+      <button @click="eraseLastDigit">&larr;</button><br />
+
       <div class="selectionOperand">
-        <input type="radio" id="one" value="Первый операнд" v-model="picked" />
-        <label for="one">Первый операнд</label>
-        <input type="radio" id="two" value="Второй операнд" v-model="picked" />
-        <label for="two">Второй операнд</label>
+        <label for="radio1" class="gender-selection__sel">Операнд 1</label>
+        <input
+          type="radio"
+          name="button_group_1"
+          id="radio1"
+          value="1"
+          @click="(activeOperand = 1), (strOperand = '')"
+        />
+        <label for="radio2" class="gender-selection__sel">Операнд 2</label>
+        <input
+          type="radio"
+          name="button_group_1"
+          id="radio2"
+          value="2"
+          @click="(activeOperand = 2), (strOperand = '')"
+        />
       </div>
     </div>
   </div>
@@ -32,13 +45,16 @@
 <script>
 export default {
   data: () => ({
+    operand: 0,
+    strOperand: "",
     operand1: 0,
     operand2: 0,
     result: 0,
     operations: ["+", "-", "/", "*", "%", "^"],
     error: "",
-    digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<-"],
+    digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     isDisplay: false,
+    activeOperand: 1,
   }),
   props: {
     msg: String,
@@ -60,6 +76,29 @@ export default {
         "^": () => Math.pow(operand1, operand2),
       };
       this.result = calcOperation[op]();
+    },
+
+    addNumber(digit) {
+      this.strOperand += digit;
+      if (this.activeOperand === 1) {
+        this.operand1 = parseInt(this.strOperand);
+      } else {
+        this.operand2 = parseInt(this.strOperand);
+      }
+
+      console.log(this.strOperand);
+    },
+
+    eraseLastDigit() {
+      let backspacedStrOperand = "";
+      backspacedStrOperand = this.strOperand.slice(0, -1);
+      this.strOperand = backspacedStrOperand;
+      if (this.activeOperand === 1) {
+        this.operand1 = parseInt(this.strOperand);
+      } else {
+        this.operand2 = parseInt(this.strOperand);
+      }
+      console.log(this.strOperand);
     },
   },
 };
